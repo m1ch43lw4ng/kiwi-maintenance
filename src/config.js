65eb962b09c5d01099cbec82f90d1aba321e6ed6
Michael <m1ch43lw4ng@berkeley.edu>
2019-03-02 10:41:16 -0800

    edited config and gitignore

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import config from "react-global-configuration";
+
+config.set({
+    apiKey: 'key6X8BebBRSfUqvE',
+    baseId: 'appgZ7x2CT2PXP2FI',
+    url: 'shryz6KcYKk2rdU1u'
+});
+
+export default config;

855b5a3b40a267cef2c28d982a6669e4a4cbfdcc
Michael <m1ch43lw4ng@berkeley.edu>
2019-03-09 18:30:50 -0800

    changed chart abstraction to react-bootstrap-table

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { render } from 'react-dom';
 import { LayoutProvider } from 'react-page-layout';
 import PublicLayout from './layouts/PublicLayout.js';
 import Chart from './pages/Chart.js';
-import Refresh from './pages/Refresh.js';
 
 const layouts = {
     'public': PublicLayout,
 };
 
 class App extends Component {
-    function
+    function;
     render() {
         return (
             <LayoutProvider layouts={layouts}>
-                <Refresh/>
+                <Chart/>
             </LayoutProvider>
         );
     }
--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -4,9 +4,42 @@ import Iframe from 'react-iframe'
 import '../index.css';
 import Airtable from 'airtable';
 import config from '../config.js';
+import Refresh from './Refresh.js';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 const base = new Airtable({ apiKey: config.get('apiKey')}).base(config.get('baseId'));
 const linkUrl = "https://airtable.com/embed/"+ config.get('url')+ "?backgroundColor=purple";
+
+const columns = [{
+    dataField: 'KiwibotID',
+    text: 'Kiwibot ID',
+    sort: true
+}, {
+    dataField: 'Status',
+    text: 'Status',
+    sort: true
+}, {
+    dataField: 'Symtoms/Diagnostic',
+    text: 'Symptoms/Diagnostic',
+    sort: true
+}, {
+    dataField: 'Accountable',
+    text: 'Accountable',
+    sort: true
+}, {
+    dataField: 'Last Updated',
+    text: 'Last Updated',
+    sort: true
+}, {
+    dataField: 'Problem',
+    text: 'Problems',
+    sort: true
+}];
+
+const defaultSorted = [{
+    dataField: 'name',
+    order: 'desc'
+}];
 
 class Chart extends Component {
 
@@ -25,39 +58,23 @@ class Chart extends Component {
                     this.setState({
                         botxreg: records
                     });
-                    fetchNextPage();
-                }
-            );
-        base('REGXNOVEDADES').select({view: 'Grid view'})
-            .eachPage(
-                (records, fetchNextPage) => {
-                    this.setState({
-                        updates: records
-                    });
-                    fetchNextPage();
                 }
             );
     }
 
     render() {
+        console.log(this.state.botxreg);
+        console.log(this.state.botxreg.map(record => RowData(record)));
         return (
             <SplitPane split="vertical" minSize={500} maxSize={1200} defaultSize={1000} allowResize={true}>
                 <div className="table-wrapper">
-                    <table className="table table-bordered table-hover table-responsive">
-                        <thead>
-                            <tr className="thead-dark">
-                                <th scope="col">Kiwibot ID</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Symptoms/Diagnostic</th>
-                                <th scope="col">Accountable</th>
-                                <th scope="col">Last Updated</th>
-                                <th scope="col">Problems</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.botxreg.map(record => <RowData{...record} />)}
-                        </tbody>
-                    </table>
+                    <BootstrapTable
+                        bootstrap4
+                        keyField="KiwibotID"
+                        data={ this.state.botxreg.map(record => RowData(record)) }
+                        columns={ columns }
+                        defaultSorted={ defaultSorted }
+                    />
                 </div>
                 <div className="overlay">
                     <Iframe url= {linkUrl}
@@ -77,13 +94,13 @@ class Chart extends Component {
 
 export default Chart;
 
-const RowData = ({id, fields, createdTime}) => (
-    <tr>
-        <th scope ="row">{fields["KiwibotID"]}</th>
-        <td>{fields["Status"]}</td>
-        <td>{fields["Symtoms/Diagnostic"]}</td>
-        <td>{fields["Accountable"]}</td>
-        <td>{fields["Last Updated"]}</td>
-        <td>{fields["Problem"]}</td>
-    </tr>
+const RowData = ({id, fields}) => (
+    {
+        'KiwibotID': fields['KiwibotID'],
+        'Status': fields['Status'],
+        'Symtoms/Diagnostic': fields['Symtoms/Diagnostic'],
+        'Accountable': fields['Accountable'],
+        'Last Updated': fields['Last Updated'],
+        'Problem': fields['Problem']
+    }
 );

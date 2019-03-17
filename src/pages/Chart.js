3eb12bf89cf98b994782fd5522d96188e3846c63
Michael <m1ch43lw4ng@berkeley.edu>
2019-03-16 17:51:12 -0700

    icon

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -8,7 +8,11 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit';
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import moment from "moment-timezone";
-import { isMobile } from "react-device-detect";
+import {
+    BrowserView,
+    MobileView,
+    isMobile,
+} from "react-device-detect";
 
 const base = new Airtable({ apiKey: config.get('apiKey')}).base(config.get('baseID'));
 const linkUrl = "https://airtable.com/embed/"+ config.get('url')+ "?backgroundColor=purple";
